e68c688fc9c5b28577df020b608ac0368004128e
admin <221807144@mycput.ac.za>
2025-05-11 15:32:47 +0200

    I edit my user class and add Amin as my class

---
+++ src/main/java/za/ac/cput/Domain/Admin.java
@@ -0,0 +1,119 @@
+package za.ac.cput.Domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String adminId;
+    private String adminName;
+    private String adminSurname;
+    private Long idNumber;
+    private String phoneNumber;
+    private String email;
+
+    public Admin() {
+    }
+
+    public Admin(Builder builder) {
+        this.adminId = builder.adminId;
+        this.adminName = builder.adminName;
+        this.adminSurname = builder.adminSurname;
+        this.idNumber = builder.idNumber;
+        this.phoneNumber = builder.phoneNumber;
+        this.email = builder.email;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public String getAdminSurname() {
+        return adminSurname;
+    }
+
+    public Long getIdNumber() {
+        return idNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminId='" + adminId + '\'' +
+                ", adminName='" + adminName + '\'' +
+                ", adminSurname='" + adminSurname + '\'' +
+                ", idNumber=" + idNumber +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String adminId;
+        private String adminName;
+        private String adminSurname;
+        private Long idNumber;
+        private String phoneNumber;
+        private String email;
+
+        public Builder setAdminId(String adminId) {
+            this.adminId = adminId;
+            return this;
+        }
+
+        public Builder setAdminName(String adminName) {
+            this.adminName = adminName;
+            return this;
+        }
+
+        public Builder setAdminSurname(String adminSurname) {
+            this.adminSurname = adminSurname;
+            return this;
+        }
+
+        public Builder setIdNumber(Long idNumber) {
+            this.idNumber = idNumber;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder copy(Admin admin) {
+            this.adminId = admin.adminId;
+            this.adminName = admin.adminName;
+            this.adminSurname = admin.adminSurname;
+            this.idNumber = admin.idNumber;
+            this.phoneNumber = admin.phoneNumber;
+            this.email = admin.email;
+            return this;
+        }
+
+        public Admin build() {
+            return new Admin(this);
+        }
+    }
+}
--- src/main/java/za/ac/cput/Domain/User.java
+++ src/main/java/za/ac/cput/Domain/User.java
@@ -1,8 +1,16 @@
 package za.ac.cput.Domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
+@Entity
 
 public class User {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
  private String UserId;
  private String UserName;
  private String UserSurname;
@@ -12,5 +20,102 @@ public class User {
  private String Email;
 
  public User(){}
+    public User(Builder builder) {
+    this.UserId = builder.UserId;
+    this.UserName = builder.UserName;
+    this.UserSurname = builder.UserSurname;
+    this.IdNumber = builder.IdNumber;
+    this.BirthDate = builder.BirthDate;
+    this.PhoneNumber = builder.PhoneNumber;
+    this.Email = builder.Email;
+    }
 
+ public String getUserId() {
+  return UserId;
+ }
+
+ public String getUserName() {
+  return UserName;
+ }
+
+ public String getUserSurname() {
+  return UserSurname;
+ }
+
+ public Long getIdNumber() {
+  return IdNumber;
+ }
+
+ public LocalDate getBirthDate() {
+  return BirthDate;
+ }
+
+ public String getPhoneNumber() {
+  return PhoneNumber;
+ }
+
+ public String getEmail() {
+  return Email;
+ }
+
+ @Override
+ public String toString() {
+  return "User{" +
+          "UserId='" + UserId + '\'' +
+          ", UserName='" + UserName + '\'' +
+          ", UserSurname='" + UserSurname + '\'' +
+          ", IdNumber=" + IdNumber +
+          ", BirthDate=" + BirthDate +
+          ", PhoneNumber='" + PhoneNumber + '\'' +
+          ", Email='" + Email + '\'' +
+          '}';
+ }
+    public static class Builder {
+    private String UserId;
+    private String UserName;
+    private String UserSurname;
+    private Long IdNumber;
+    private LocalDate BirthDate;
+    private String PhoneNumber;
+    private String Email;
+
+    public Builder setUserId(String userId) {
+    this.UserId = userId;
+    return this;
+    }
+
+    public Builder setUserName(String userName) {
+    this.UserName = userName;
+    return this;
+    }
+
+    public Builder setUserSurname(String userSurname) {
+    this.UserSurname = userSurname;
+    return this;
+    }
+
+    public Builder setIdNumber(Long idNumber) {
+    this.IdNumber = idNumber;
+    return this;
+    }
+
+    public Builder setBirthDate(LocalDate birthDate) {
+    this.BirthDate = birthDate;
+    return this;
+    }
+
+    public Builder setPhoneNumber(String phoneNumber) {
+    this.PhoneNumber = phoneNumber;
+    return this;
+    }
+
+    public Builder setEmail(String email) {
+    this.Email = email;
+    return this;
+    }
+
+    public User build() {
+    return new User(this);
+    }
+    }
 }
