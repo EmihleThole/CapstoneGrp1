5d3e3eb3c8b43ce02ab3b83615d259b555476660
admin <221807144@mycput.ac.za>
2025-05-11 14:03:31 +0200

    just add userDomain

---
+++ src/main/java/za/ac/cput/Domain/User.java
@@ -0,0 +1,16 @@
+package za.ac.cput.Domain;
+
+import java.time.LocalDate;
+
+public class User {
+ private String UserId;
+ private String UserName;
+ private String UserSurname;
+ private Long IdNumber;
+ private LocalDate BirthDate;
+    private String PhoneNumber;
+ private String Email;
+
+ public User(){}
+
+}
